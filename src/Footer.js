195607a86e0c33e2dee4fb674f744b3e55e5a528
Gage Norton <nortongage0@gmail.com>
2023-05-07 16:29:09 -0500

    Add footer styles

--- src/Footer.js
+++ src/Footer.js
@@ -1,10 +1,33 @@
+import { Icon } from './Icon';
+
 const Footer = () => {
 
-    return (
-      <div className="footer">
-       <h1>this is the Footer!</h1>
-      </div>
-    );
-  }
-   
-  export default Footer;
+  return (
+    <footer className="footer">
+      <a href="https://github.com/Gatewayss" target="_blank" rel="noreferrer">
+      <Icon
+        iconName="Github"
+        size={30}
+        className="icons"
+      />
+      </a>
+      <a href="https://www.linkedin.com/in/gage-norton-a3568725b/" target="_blank" rel="noreferrer">
+      <Icon
+        iconName="Linkedin"
+        size={30}
+        className="icons"
+      />
+      </a>
+      <a href="https://www.instagram.com/gateway___/" target="_blank" rel="noreferrer">
+      <Icon
+        iconName="Instagram"
+        size={30}
+        className="icons"
+      />
+      </a>
+      <p>click these!</p>
+    </footer>
+  );
+}
+
+export default Footer;
