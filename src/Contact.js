309af789bff5d08b46397c2632cedd9a0d569c40
Gage Norton <nortongage0@gmail.com>
2023-05-07 13:57:36 -0500

    Change input attributes

--- src/Contact.js
+++ src/Contact.js
@@ -1,10 +1,29 @@
 const Contact = () => {
 
-    return (
-      <div className="contact">
-       <h1>this is the contact page!</h1>
-      </div>
-    );
-  }
-   
-  export default Contact;
+  return (
+    <div className="contact">
+      <h2>Contact</h2>
+      <p>File </p>
+      <form type="submit">
+        <label>Name:</label><br />
+        <input
+          type="text"
+          id="name"
+          required /><br />
+        <label>Email:</label><br />
+        <input
+          type="email"
+          id="email"
+          required /><br />
+        <label>Message:</label><br />
+        <textarea
+          type="text"
+          id="message"
+          required /><br />
+        <button>Send!</button>
+      </form>
+    </div>
+  );
+}
+
+export default Contact;
