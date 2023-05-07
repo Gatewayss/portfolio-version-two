5dd5f507602d9a2af4f5fed000c85e143942fd63
Gage Norton <nortongage0@gmail.com>
2023-05-07 18:36:14 -0500

    Add projects and assets

--- src/Portfolio.js
+++ src/Portfolio.js
@@ -1,9 +1,19 @@
 const Portfolio = () => {
 
     return (
-      <div className="portfolio">
-       <h1>this is the portfolio page!</h1>
-      </div>
+      <div class="portfolio">
+          <a href="https://retro-great-1.herokuapp.com/">
+            <img src={require("./assets/age-filter.gif")} alt="retro great portfolio" />
+          </a>
+
+          <a href="https://github.com/Gatewayss/gage-api-quiz">
+            <img src={require("./assets/Gage's Quiz (1).gif")} alt="trans history quiz" />
+          </a>
+
+          <a href="https://gatewayss.github.io/recipes-for-disaster/">
+            <img src={require("./assets/Recipes for disaster.gif")} alt="Recipes for disaster tutorial" />
+          </a>
+        </div>
     );
   }
    
