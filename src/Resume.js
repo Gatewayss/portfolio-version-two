a0ee41e66f68a121190fbdb8e55df068023ae70f
Gage Norton <nortongage0@gmail.com>
2023-05-07 18:18:31 -0500

    Add base resume styles

--- src/Resume.js
+++ src/Resume.js
@@ -2,7 +2,34 @@ const Resume = () => {
 
     return (
       <div className="resume">
-       <h1>this is the resume page!</h1>
+       <h2>Proficient Skills</h2>
+       <p>View my resume <a href="https://docs.google.com/document/d/1v_aytDLpXom_-q7-naa_kQuSy6T2IIW4zV5Pwdi2Q3E/edit?usp=sharing.pdf" download>here!</a></p>
+       <ul>
+        <li>Javascript</li>
+        <li>CSS</li>
+        <li>SASS</li>
+        <li>HTML5</li>
+        <li>Git</li>
+        <li>MySQl</li>
+        <li>Git</li>
+        <li>APIs</li>
+        <li>REST</li>
+        <li>Web Design</li>
+        <li>search engine Optimization (SEO)</li>
+        <li>Github</li>
+        <li>SQL</li>
+        <li>Node.js</li>
+        <li>react</li>
+        <li>GraphQL</li>
+        <li>Back-end development</li>
+        <li>Front-end development</li>
+        <li>Bootstrap</li>
+        <li>AWS</li>
+        <li>PWA</li>
+        <li>JSON</li>
+        <li>MongoDB</li>
+        <li>JQuery</li>
+       </ul>
       </div>
     );
   }
