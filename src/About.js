0730cd99739c41f16b89b4213a68e19fba74a1bd
Gage Norton <nortongage0@gmail.com>
2023-05-06 19:39:49 -0500

    Add about me section

--- src/About.js
+++ src/About.js
@@ -1,10 +1,11 @@
 const About = () => {
 
-    return (
-      <div className="about">
-       <h1>this is the about page!</h1>
-      </div>
-    );
-  }
-   
-  export default About;
+  return (
+    <div className="about">
+      <h2>About Me</h2>
+      <p>As a passionate learner and aspiring web developer, I am a recent graduate of U of M's full-stack web boot camp, pursuing my dream career. With almost ten years of experience in the food service industry, I was looking for a change that would require more flexible hours and a different set of problem-solving skills. Since discovering coding through friends in this field, I have developed a passion for web development. I am excited to continue developing my skills and exploring the ever-evolving world of web development.</p>
+    </div>
+  );
+}
+
+export default About;
